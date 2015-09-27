265ff3fc35dad5ffd5c38cdc0c8d7869c1bd3368
Michael <metaperture@users.noreply.github.com>
2015-09-26 22:21:44 -0400

    Update install_awesome_vimrc.sh

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -1,14 +1,14 @@
-cd ~/.vim_runtime
+cd ~/.dotfiles/vimrc
 
-echo 'set runtimepath+=~/.vim_runtime
+echo 'set runtimepath+=~/.dotfiles/vimrc
 
-source ~/.vim_runtime/vimrcs/basic.vim
-source ~/.vim_runtime/vimrcs/filetypes.vim
-source ~/.vim_runtime/vimrcs/plugins_config.vim
-source ~/.vim_runtime/vimrcs/extended.vim
+source ~/.dotfiles/vimrc/vimrcs/basic.vim
+source ~/.dotfiles/vimrc/vimrcs/filetypes.vim
+source ~/.dotfiles/vimrc/vimrcs/plugins_config.vim
+source ~/.dotfiles/vimrc/vimrcs/extended.vim
 
 try
-source ~/.vim_runtime/my_configs.vim
+source ~/.dotfiles/vimrc/my_configs.vim
 catch
 endtry' > ~/.vimrc
 
